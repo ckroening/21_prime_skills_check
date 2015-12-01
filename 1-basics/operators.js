ddad56fca9e67e6c64873b11454b22d62af543d7
ckroening <charlottekroening@gmail.com>
2015-12-01 15:52:35 -0600

    1-basics complete.

--- 1-basics/operators.js
+++ 1-basics/operators.js
@@ -1,7 +1,8 @@
 // 1 - Assign the value 22 to a variable called eta
-
+var eta = 22;
 // 2 - Create a new variable called peeps and assign it the value of eta
-
+var peeps = eta;
 // 3 - Decrease the value of peeps by 1
-
+eta - 1;	
 // 4 - Write a console log that checks if eta is equal to peeps 
+console.log(eta === peeps);
