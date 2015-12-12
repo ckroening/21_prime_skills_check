ada447c77e5f485c05f6f5f3451c6a608cc7f125
ckroening <charlottekroening@gmail.com>
2015-12-12 16:23:25 -0600

    7-conditionals complete.

--- 7-conditionals/switch.js
+++ 7-conditionals/switch.js
@@ -2,3 +2,22 @@
 // console logs witty statements for each
 // day of the standard work week: Monday, Tuesday,
 // Wednesday, Thursday, Friday
+var day = "Thursday";
+
+switch (day) {
+	case "Monday":
+	console.log("Monday is Monday");
+	break;
+	case "Tuesday":
+	console.log("Tuesday is Tuesday");
+	break;
+	case "Wednesdy":
+	console.log("Wednesday is Wednesday");
+	break;
+	case "Thursday":
+	console.log("Thursday is Thursday");
+	break;
+	case "Friday":
+	console.log("Friday is Friday");
+};
+
