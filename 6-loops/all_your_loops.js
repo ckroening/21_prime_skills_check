7c757eebb8965d18b79e339296980fd6ef90aa6c
ckroening <charlottekroening@gmail.com>
2015-12-11 18:28:58 -0600

    6-loops complete.

--- 6-loops/all_your_loops.js
+++ 6-loops/all_your_loops.js
@@ -2,11 +2,25 @@
 // Each iteration will increase the value
 // of checkz by 1
 var checkz = 0;
+for(var i = 0; i <= 5; i++) {
+	checkz++;
+	console.log(checkz);
+}
 
 // 2 - Write a do...while loop that runs
 // 5 times. Each iteration will increase
 // the value of checkz by 1
+do {
+   checkz += 1;
+   console.log(checkz);
+} while (i < 6);
 
 // 3 - Write a while loop that runs
 // 3 times. Each iteration will decrease
 // the value of checkz by -2
+var n = 0;
+while (n < 3) {
+	n++;
+	checkz -= 2;
+	console.log(checkz);
+}
