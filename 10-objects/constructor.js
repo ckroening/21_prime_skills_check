c983b9bd6b67bc1b087c5c98e9bc7f064ff7e7a7
ckroening <charlottekroening@gmail.com>
2015-12-13 15:52:16 -0600

    10-objects complete (except for adding functions/methods).

--- 10-objects/constructor.js
+++ 10-objects/constructor.js
@@ -7,6 +7,25 @@
 // - area returns area of triangle
 // - perimeter returns perimeter of triangle
 
+var area = function(base, side1) {
+	return 0.5*(base*side);
+};
+
+function RightTriangle(side1,side2,base) {
+	this.side1 = side1;
+	this.side2 = side2;
+	this.base = base;
+	this.area = area;
+	this.perimeter = function(base, side1, side2) {
+		return side1 + side2 + base;
+	};
+};
+
 // 2 - Create two instances of the RightTriangle
 // object. Console log the results of calling
 // both functions on both instances.
+var misterTriangle = new RightTriangle(1, 3, 5);
+console.log(misterTriangle);
+
+var missTriangle = new RightTriangle(3, 5, 2);
+console.log(missTriangle);
--- 10-objects/literal.js
+++ 10-objects/literal.js
@@ -5,15 +5,28 @@
 // - zip
 // Assign the values based on Prime's
 // address.
+var prime = {
+	city: "Bloomington",
+	state: "MN",
+	zip: 55431
+};
 
 // 2 - Using dot notation, create a
 // new property for prime. This property
 // will be called address and will 
 // take the value of Prime's street
 // address.
+prime.address = "9401 James Ave S #152";
+//console.log(prime);
 
 // 3 - Using dot notation, create a
 // new method for prime. This method
 // will console log out the complete
 // address of prime. Call the new
 // property/method print.
+
+var print = function() {
+	console.log(prime);
+};
+
+prime.print = print;
